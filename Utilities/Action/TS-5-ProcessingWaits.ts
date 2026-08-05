62b891a0423585816c48ac0dbb8fa9d982ff16e9
Leapwork Play <noreply+leapwork-play@leapwork.local>
2026-08-05 18:20:42 +0530

    Update Utilities/Action/TS-5-ProcessingWaits.ts from Leapwork Play

---
+++ Utilities/Action/TS-5-ProcessingWaits.ts
@@ -0,0 +1,28 @@
+import type { Page } from "@playwright/test";
+
+export async function waitUntilPleaseWaitgPopupDisappears(
+    page: Page,
+    timeoutMs = 45000,
+): Promise<void> {
+    
+    const processingPopup = page.getByText("Please wait. We're processing your request.", {
+        exact: false,
+    }).last();
+
+    await processingPopup.waitFor({
+        state: "hidden",
+        timeout: timeoutMs,
+    });
+}
+export async function waitUntilProcessingOperationPopupDisappears(
+    page: Page,
+    timeoutMs = 45000,
+): Promise<void> {
+    const processingOperationPopup = page.getByText(new RegExp("Processing operation", "i")).last();
+
+    await processingOperationPopup.waitFor({
+        state: "hidden",
+        timeout: timeoutMs,
+    });
+   
+}
